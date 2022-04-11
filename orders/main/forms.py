36ff6c65e80496bfef3d121e8b375dabc3608049
Mateusz Szawczenko <mateusz.szawczenko@g4e.pl>
2022-04-11 20:53:48 +0200

    added forms validation and set everything up to publish this app on heroku

--- orders/main/forms.py
+++ orders/main/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from django.forms import TextInput
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class CreateNewProduct(forms.Form):
-    name = forms.CharField(label="Name", max_length=200)
-    amount_in_stock = forms.DecimalField(max_digits=6, decimal_places=0)
+class CreateNewBook(forms.Form):
+
+    title = forms.CharField(max_length=200, required=True, label="", widget=TextInput(attrs={'placeholder': 'Title'}))
+    author = forms.CharField(max_length=200, required=True, label="", widget=TextInput(attrs={'placeholder': 'Author'}))
+    date_published = forms.IntegerField(required=True, min_value=0, max_value=10000, label="",
+                                        widget=TextInput(attrs={'placeholder': 'Publication Year'}),
+                                        error_messages={"min_value": "Year publication must be a positive integer."})
+    isbn_number = forms.CharField(required=True, max_length=50, label="", widget=TextInput(attrs={'placeholder': 'ISBN'}))
+    pages_number = forms.IntegerField(required=True, min_value=0, max_value=10000, label="",
+                                      widget=TextInput(attrs={'placeholder': 'Pages number'}),
+                                      error_messages={"min_value": "Year publication must be a positive integer."})
+    page_title_href = forms.URLField(required=True, max_length=1000, label="", widget=TextInput(attrs={'placeholder': 'Title Page Link'}))
+    language_published = forms.CharField(required=True, max_length=100, label="", widget=TextInput(attrs={'placeholder': 'Language'}))
