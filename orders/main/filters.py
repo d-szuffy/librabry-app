ad7400b076ed2d6b68c760b83f43fa239e785724
Mateusz Szawczenko <mateusz.szawczenko@g4e.pl>
2022-04-11 12:29:29 +0200

    completely new, more modern design. Also addes some more POST methods tests.

--- orders/main/filters.py
+++ orders/main/filters.py
@@ -2,15 +2,17 @@ import django_filters
 
 from .models import ListOfBooks, Book
 from django_filters import DateFilter, CharFilter
+from django.forms.widgets import TextInput
 
 
 class BookFilter(django_filters.FilterSet):
 
-    start_date = django_filters.NumberFilter(field_name="date_published", lookup_expr="gte")
-    end_date = django_filters.NumberFilter(field_name="date_published", lookup_expr="lte")
+    start_date = django_filters.NumberFilter(field_name="date_published", lookup_expr="gte", label="", widget=TextInput(attrs={'placeholder': 'Date from'}))
+    end_date = django_filters.NumberFilter(field_name="date_published", lookup_expr="lte", label="", widget=TextInput(attrs={'placeholder': 'Date to'}))
 
-    title = CharFilter(field_name="title", lookup_expr="icontains")
-    author = CharFilter(field_name="author", lookup_expr="icontains")
+    title = CharFilter(field_name="title", lookup_expr="icontains", label="", widget=TextInput(attrs={'placeholder': 'Title contains'}))
+    author = CharFilter(field_name="author", lookup_expr="icontains", label="", widget=TextInput(attrs={'placeholder': 'Author contains'}))
+    language_published = CharFilter(field_name="language_published", lookup_expr="icontains", label="", widget=TextInput(attrs={'placeholder': 'Language'}))
 
     class Meta:
         model = Book
--- orders/main/tests/test_views.py
+++ orders/main/tests/test_views.py
@@ -10,7 +10,7 @@ class TestView(TestCase):
         self.home_url = reverse('home')
         self.create_new_records_url = reverse('create')
         self.view_all_books_url = reverse('list')
-        self.import_books = reverse('import')
+        self.import_books_url = reverse('import')
         self.list_of_books_1 = ListOfBooks.objects.create(
             name="TestList"
         )
@@ -34,7 +34,51 @@ class TestView(TestCase):
         self.assertTemplateUsed(response, 'main/books.html')
 
     def test_import_books_from_google_api_all_books_GET(self):
-        response = self.client.get(self.import_books)
+        response = self.client.get(self.import_books_url)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/google_api.html')
+
+    def test_create_new_records_in_db_POST_add_new_book(self):
+        Book.objects.create(
+            listofbooks=self.list_of_books_1,
+            title="Test Book",
+            author="Mateusz Szawczenko",
+            date_published=2022,
+            isbn_number="97082504494",
+            pages_number=400,
+            page_title_href="https://www.dragonball-multiverse.com/pl/page-1995.html#h_read",
+            language_published="pl"
+        )
+
+        response = self.client.post(self.create_new_records_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(self.list_of_books_1.book_set.first().title, "Test Book")
+
+    def test_create_new_records_in_db_POST_no_data(self):
+        response = self.client.post(self.create_new_records_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.list_of_books_1.book_set.count(), 0)
+
+    def test_create_new_records_in_db_POST_delete_all_records(self):
+        Book.objects.create(
+            listofbooks=self.list_of_books_1,
+            title="Test Book",
+            author="Mateusz Szawczenko",
+            date_published=2022,
+            isbn_number="97082504494",
+            pages_number=400,
+            page_title_href="https://www.dragonball-multiverse.com/pl/page-1995.html#h_read",
+            language_published="pl"
+        )
+
+        for item in self.list_of_books_1.book_set.all():
+            item.delete()
+
+        response = self.client.post(self.create_new_records_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.list_of_books_1.book_set.count(), 0)
